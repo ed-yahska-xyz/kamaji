650a2ef6206d947f2d28e1e8e243d5d2565d55df
Akshay <ednihs.yahska@gmail.com>
2025-12-28 22:06:52 +0530

    reader PAT secret from docker secrets path if available

--- src/services/github/index.ts
+++ src/services/github/index.ts
@@ -1,4 +1,6 @@
+import { readFileSync, existsSync } from "fs";
 
+const path = "/run/secrets/contributions_pat";
 
 const params = {
     endpoint: "https://api.github.com/graphql",
@@ -26,14 +28,19 @@ export async function getContributions(): Promise<any> {
             }
         }
     `;
+    const contributionsPat = existsSync(path) ? readFileSync(path, "utf8").trim() : process.env.CONTRIBUTIONS_PAT;
     const response = await fetch(params.endpoint, {
         method: "POST",
         headers: {
-            "Authorization": `Bearer ${process.env.CONTRIBUTIONS_PAT}`,
+            "Authorization": `Bearer ${contributionsPat}`,
             "Content-Type": "application/json",
         },
         body: JSON.stringify({ query })
     });
+
+    if (!response.ok) {
+        console.error("Error: ", response.statusText);
+    }
 
     return response.json();
 }
